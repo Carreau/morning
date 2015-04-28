6d929bcd972b89b6f083775e3ccf026d5a341406
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-04-28 13:54:36 -0700

    entr point plus gfo

--- morning/__init__.py
+++ morning/__init__.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
+"""
+A simple module to do repetitive tasks in the morning. 
+
+Targeted as updating git repos.
+
+"""
 
 __author__ = 'Matthias Bussonnier'
 __email__ = 'bussonniermatthias@gmail.com'
 __version__ = '0.1.0'
+
+version = __version__
 
 import sys
 import os
@@ -10,6 +18,10 @@ from os.path import expanduser
 import configparser
 import io
 import argparse
+
+import logging
+log = logging.getLogger()
+log.setLevel(10)
 
 class _config(object):
 
@@ -30,15 +42,15 @@ def main():
 
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title='subcommands', dest='subcmd')
-    parser_add = subparsers.add_parser('add',
-            help="register a package on PyPI without uploading any files"
-    )
+    parser_add = subparsers.add_parser('add',help='')
+    parser_list = subparsers.add_parser('list',help='')
+    
 
     parser_add.add_argument('dir')
 
     args = parser.parse_args()
 
-    if args.dir:
+    if args.subcmd == 'dir':
         directory = os.path.abspath(expanduser(args.dir))
         if not os.path.isdir(directory):
             sys.exit('%s is not a directory'%directory)
@@ -46,6 +58,21 @@ def main():
             if not 'mornings' in config.sections():
                 config['mornings'] = {}
             config['mornings'][directory] = 'true'
+
+    elif args.subcmd == 'list':
+        with _config() as config:
+            for k in config['mornings'].keys():
+                print(k)
+                log.debug('%s' %(k))
+
+    else :
+        from subprocess import Popen
+        log.info('no arguments, will update all things.')
+        with _config() as config:
+            for k in config['mornings'].keys():
+                print('will update git in {}'.format(k))
+                Popen(['git','fetch','origin'],cwd=k)
+
 
 
 
