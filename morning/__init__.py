65b3ccc3cf48faa557508911c79b37b8078c0cdf
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-04-28 15:07:30 -0700

    bump to 1.1

--- morning/__init__.py
+++ morning/__init__.py
@@ -8,7 +8,7 @@ Targeted as updating git repos.
 
 __author__ = 'Matthias Bussonnier'
 __email__ = 'bussonniermatthias@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 version = __version__
 
