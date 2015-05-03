a2891bd20a0fbf32f9ad89dbccf0407572c117e2
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-05-02 19:32:58 -0700

    test revert

--- morning/__init__.py
+++ morning/__init__.py
@@ -8,7 +8,7 @@ Targeted as updating git repos.
 
 __author__ = 'Matthias Bussonnier'
 __email__ = 'bussonniermatthias@gmail.com'
-__version__ = '0.1.4'
+__version__ = '0.1.4.1'
 
 version = __version__
 
