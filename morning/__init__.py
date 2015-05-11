229d4d448f5dc6bf591fd366dcbbdce42f44c9f7
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-05-11 16:08:40 -0700

    run in subprocesses and 1.5

--- morning/__init__.py
+++ morning/__init__.py
@@ -8,7 +8,7 @@ Targeted as updating git repos.
 
 __author__ = 'Matthias Bussonnier'
 __email__ = 'bussonniermatthias@gmail.com'
-__version__ = '0.1.4.1'
+__version__ = '0.1.5'
 
 version = __version__
 
@@ -19,6 +19,7 @@ from os.path import expanduser
 import configparser
 import io
 import argparse
+from multiprocessing import Pool
 
 import logging
 log = logging.getLogger(__file__)
@@ -81,45 +82,58 @@ def main():
     else :
         status(gfo=True)
         log.info('no arguments, will update all the things.')
+\
+def each_repo(k):
+    gfo = True
+    from subprocess import Popen, run, DEVNULL, PIPE
+    #log.info('will update git in {}'.format(k))
+    fast_forward = False
+    if not os.path.exists(k):
+        log.info('skipping %s, that does not exists'%k)
+        return
+    if gfo:
+        res = run(['git','fetch','origin'],cwd=k, stdout=PIPE, stderr=PIPE)
+        if res.returncode is not 0 :
+            log.error("could not fetch %s"%k)
+            return
+        c = configparser.ConfigParser()
+        c.read(os.path.join(k,'.git/config'))
+        fast_forward = (c.get('morning', 'fast-forward', fallback=None) == 'True')
+        log.debug("configured for auto fast-forward %s" % fast_forward)
+    res = run('git rev-parse --abbrev-ref HEAD'.split(), cwd=k, stdout=PIPE, stderr=PIPE)
+    branch = res.stdout.decode().strip()
+    res = run('git status -sb --porcelain'.split(), cwd=k, stdout=PIPE, stderr=PIPE)
+    m = re.findall('behind ([0-9]+)', res.stdout.splitlines()[0].decode());
+    behind = int(m[0]) if m else 0
+    m = re.findall('ahead ([0-9]+)', res.stdout.splitlines()[0].decode());
+    ahead = int(m[0]) if m else 0
+    if behind and not ahead and branch == 'master':
+        extra = 'can be fast-forwarded'
+        if fast_forward: 
+            log.debug('is ffding')
+            res = run(['git','merge','origin/master','--ff-only'], cwd=k, stdout=PIPE, stderr=PIPE)
+            if res.returncode is not 0 :
+                log.error(res.stderr.decode())
+            extra = ' [fast-forwarded]'
+    else:
+        extra = ''
+    res = run(['travis','status','--no-interactive'],cwd=k, stdout=PIPE, stderr=PIPE)
+    extra = extra + res.stdout.decode().split('\n')[0]
+
+    log.info('{:40s} on branch {:7s} -{:02d},+{:02d}, {:s}'.format(compressuser(k),branch, behind, ahead, extra))
 
 def status(gfo=False):
-    from subprocess import Popen, run, DEVNULL, PIPE
+    p = Pool(15)
     with _config() as config:
-        for k in config['mornings'].keys():
-            #log.info('will update git in {}'.format(k))
-            fast_forward = False
-            if gfo:
-                res = run(['git','fetch','origin'],cwd=k, stdout=PIPE, stderr=PIPE)
-                if res.returncode is not 0 :
-                    log.error("could not fetch %s"%k)
-                    continue
-                c = configparser.ConfigParser()
-                c.read(os.path.join(k,'.git/config'))
-                fast_forward = (c.get('morning', 'fast-forward', fallback=None) == 'True')
-                log.debug("configured for auto fast-forward %s" % fast_forward)
-            res = run('git rev-parse --abbrev-ref HEAD'.split(), cwd=k, stdout=PIPE, stderr=PIPE)
-            branch = res.stdout.decode().strip()
-            res = run('git status -sb --porcelain'.split(), cwd=k, stdout=PIPE, stderr=PIPE)
-            m = re.findall('behind ([0-9]+)', res.stdout.splitlines()[0].decode());
-            behind = int(m[0]) if m else 0
-            m = re.findall('ahead ([0-9]+)', res.stdout.splitlines()[0].decode());
-            ahead = int(m[0]) if m else 0
-            if behind and not ahead and branch == 'master':
-                extra = 'can be fast-forwarded'
-                if fast_forward: 
-                    log.debug('is ffding')
-                    res = run(['git','merge','origin/master','--ff-only'], cwd=k, stdout=PIPE, stderr=PIPE)
-                    if res.returncode is not 0 :
-                        log.error(res.stderr.decode())
-                    extra = ' [fast-forwarded]'
-            else:
-                extra = ''
-            res = run(['travis','status','--no-interactive'],cwd=k, stdout=PIPE, stderr=PIPE)
-            extra = extra + res.stdout.decode().split('\n')[0]
-
-            log.info('{:50s} on branch {:7s} -{:02d},+{:02d}, {:s}'.format(k,branch, behind, ahead, extra))
+        #l = lambda x: each_repo(gfo,x)
+        p.map(each_repo,config['mornings'].keys())
 
 
+def compressuser(path):
+    from os.path import expanduser as eu
+    if path.startswith(eu('~').lower()):
+        return path.replace(eu('~').lower(),"~",1)
+    return path
 
 
 
